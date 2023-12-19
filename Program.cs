65da0417a318db9fdfee35af9db74a7e9cc95527
Kirill <sosisks888@mail.ru>
2023-12-19 11:07:51 +0300

    delete test user

--- Program.cs
+++ Program.cs
@@ -4,11 +4,13 @@ using Test.Areas.Identity.Data;
 using Test.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<TestContext>(options => options.UseNpgsql(connectionString));
 
-builder.Services.AddDefaultIdentity<TestUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<TestContext>();
+builder.Services.AddDefaultIdentity<TestUser>(options => 
+	options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<TestContext>();
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
